48bbc0daa1c5f77ba9c26e44439afffa458bf97d
benciuf <florin.benciu@inoe.ro>
2021-12-25 20:59:09 +0100

    fin
    fin

--- twoSumProblem.py
+++ twoSumProblem.py
@@ -1,13 +1,16 @@
-#The two sum problem is a common interview question, and it is a variation of the subset sum problem. For example, if the array is [3, 5, 2, -4, 8, 11] and the sum is 7, your program should return [[11, -4], [2, 5]] because 11 + -4 = 7 and 2 + 5 = 7.
+#The two sum problem is a common interview question, and it is a variation of the
+#subset sum problem. For example, if the array is [3, 5, 2, -4, 8, 11] and the sum is 7,
+#your program should return [[11, -4], [2, 5]] because 11 + -4 = 7 and 2 + 5 = 7.
 
 x = (3,5,2,-4,8,11)
-sumSearch = 7
-sumTemp = 0
-def getTwoSum(x):
-	for i in x:
-		for j in range (2,length(x)):
-			sumTemp = x[i] + x[j]
-			if(sumTemp = SumSearch):
-				return (x[i],x[j],", ")
-				
-print(getTwoSum(x))
+sumSearch = 13
+def getTwoSum():
+    for i in range (0,len(x)):
+        for j in range (i+1,len(x)):
+            #print("i = ",i)
+            #print("j = ",j)
+            sumTemp = x[i] + x[j]
+            #print("sumTemp = ",x[i], " + ",x[j], " = ", sumTemp)
+            if(sumTemp == sumSearch):
+                print("[",x[i],", ",x[j],"],", end=' ')
+getTwoSum()
