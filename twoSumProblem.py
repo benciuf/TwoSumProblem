58c347f77d528c6a80295bf2a0d4eac03a77d27e
benciuf <florin.benciu@inoe.ro>
2021-12-25 20:31:10 +0100

    Add python code
    Sum will have the first element with the length(vector)- 1

---
+++ twoSumProblem.py
@@ -0,0 +1,13 @@
+#The two sum problem is a common interview question, and it is a variation of the subset sum problem. For example, if the array is [3, 5, 2, -4, 8, 11] and the sum is 7, your program should return [[11, -4], [2, 5]] because 11 + -4 = 7 and 2 + 5 = 7.
+
+x = (3,5,2,-4,8,11)
+sumSearch = 7
+sumTemp = 0
+def getTwoSum(x):
+	for i in x:
+		for j in range (2,length(x)):
+			sumTemp = x[i] + x[j]
+			if(sumTemp = SumSearch):
+				return (x[i],x[j],", ")
+				
+print(getTwoSum(x))
